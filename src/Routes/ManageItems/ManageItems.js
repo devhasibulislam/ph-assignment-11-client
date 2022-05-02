a8cb585a352fe3ba93e1c91965385c2fddc1c910
hasibulislam999 <hasib143sl@gmail.com>
2022-05-03 01:33:17 +0600

    pagination done

--- src/Routes/ManageItems/ManageItems.js
+++ src/Routes/ManageItems/ManageItems.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import useProducts from '../../hooks/useProducts';
 import Card from '../Card/Card';
 
 const ManageItems = () => {
@@ -10,21 +9,23 @@ const ManageItems = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:5000/product')
+        axios.get(`http://localhost:5000/product?pageNumber=${activePage}&viewItems=${viewCount}`)
             .then(res => setProducts(res?.data))
-    }, []);
+    }, [activePage, viewCount]);
 
     useEffect(() => {
         axios.get('http://localhost:5000/productCount')
             .then(res => {
                 const overallNumbers = res?.data?.count;
-                const defaultTen = Math.ceil(overallNumbers / 10);
+                const defaultTen = Math.ceil(overallNumbers / viewCount);
                 setTotalProductCount(defaultTen);
             })
-    }, []);
+    }, [viewCount]);
+
     return (
         <div className='bg-gray-400 py-4'>
             <h1 className='text-center text-6xl py-4'># Our Services</h1>
+            <hr className='w-36 mx-auto border-t-4 border-[#00a1e5]' />
             <div className='grid grid-cols-3 max-w-7xl mx-auto gap-y-12 py-12'>
                 {
                     products.map(product => <Card
@@ -39,7 +40,9 @@ const ManageItems = () => {
                         {
                             [...Array(totalProductCount).keys()]
                                 .map(
-                                    productsPageNumber => <li>
+                                    productsPageNumber => <li
+                                        key={productsPageNumber}
+                                    >
                                         <span className={
                                             productsPageNumber === activePage
                                                 ?
@@ -55,11 +58,11 @@ const ManageItems = () => {
                                 )
                         }
                     </ul>
-                    <div class="flex justify-center">
-                        <div class="my-3 xl:w-96">
+                    <div className="flex justify-center">
+                        <div className="my-3 xl:w-96">
                             <select
                                 onChange={(e) => setViewCount(e.target.value)}
-                                class="
+                                className="
                                     text-center
                                     form-select appearance-none
                                     block
@@ -78,7 +81,7 @@ const ManageItems = () => {
                                     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                                 "
                                 aria-label="Default select example">
-                                <option selected>Select, how much items wanna see?</option>
+                                <option value="10" selected>Selected default or how much wanna see?</option>
                                 <option value="15">Fifteen</option>
                                 <option value="20">Twenty</option>
                                 <option value="30">Thirty</option>
