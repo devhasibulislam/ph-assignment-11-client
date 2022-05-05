e1aa1dfcdde24d48e1daffd80a64d9d88e8051c8
hasibulislam999 <hasib143sl@gmail.com>
2022-05-05 19:13:22 +0600

    class name updated

--- src/Pages/TableView/TableView.js
+++ src/Pages/TableView/TableView.js
@@ -15,29 +15,29 @@ const TableView = () => {
                 </span>
             }</h1>
             <hr className='w-36 mx-auto border-t-4 mb-8 border-[#00a1e5] rounded' />
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Product ID
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 User Email
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Product Image
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Product Name
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Price
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Supplier
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 QTY
                             </th>
                         </tr>
--- src/Pages/Tcard/Tcard.js
+++ src/Pages/Tcard/Tcard.js
@@ -3,26 +3,26 @@ import React from 'react';
 const Tcard = (props) => {
     const { _id, name, img, price, qty, email, supplier } = props.item;
     return (
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
                 {_id}
             </th>
-            <td class="px-6 py-4">
+            <td className="px-6 py-4">
                 {email}
             </td>
-            <td class="px-6 py-4">
+            <td className="px-6 py-4">
                 <img src={img} alt="product-icon" className='w-8 rounded' />
             </td>
-            <td class="px-6 py-4">
+            <td className="px-6 py-4">
                 {name}
             </td>
-            <td class="px-6 py-4 font-bold">
+            <td className="px-6 py-4 font-bold">
                 {price}
             </td>
-            <td class="px-6 py-4">
+            <td className="px-6 py-4">
                 {supplier}
             </td>
-            <td class="px-6 py-4">
+            <td className="px-6 py-4">
                 {qty}
             </td>
         </tr>
