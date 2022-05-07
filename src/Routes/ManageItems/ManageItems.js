43c6c1ce42842e6fd6bf1b6809501acf2b574010
hasibulislam999 <hasib143sl@gmail.com>
2022-05-07 20:21:44 +0600

    bug fix

--- src/Routes/ManageItems/ManageItems.js
+++ src/Routes/ManageItems/ManageItems.js
@@ -77,7 +77,7 @@ const ManageItems = () => {
                                                 ?
                                                 "first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-pink-500 text-white bg-pink-500 cursor-pointer"
                                                 :
-                                                "first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-pink-500 bg-white text-pink-500 cursor-pointer"
+                                                "first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-pink-500 bg-white text-pink-500 cursor-pointer hover:scale-110 transition-all"
                                         }
                                             onClick={() => setActivePage(productsPageNumber)}
                                         >
